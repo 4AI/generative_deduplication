5a1fee8421a9e420e69a10ded6d730b4cd797a37
Sean <xmlee97@gmail.com>
2024-10-08 16:39:16 +0800

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gen-dedup",
-    version='0.0.1',
+    version='0.0.2',
     author="Sean Lee",
     author_email="xmlee97@gmail.com",
     description="Generative deduplication",
